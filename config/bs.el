05fed0b4de606dd25bd978ea15abfb52537ae3c7
byungsoolee <mp@byungsooleeui-MacBook-Pro.local>
2016-08-10 19:00:58 +0900

    package upate and custom el add

--- config/bs.el
+++ config/bs.el
@@ -1,2 +1,24 @@
+(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
+                         ("marmalade" . "https://marmalade-repo.org/packages/")
+                         ("melpa" . "https://melpa.org/packages/")))
+
 (eval-after-load 'php-mode
   '(require 'php-ext))
+
+;; tab unindent set
+(global-set-key (kbd "<backtab>") 'un-indent-by-removing-4-spaces)
+(defun un-indent-by-removing-4-spaces ()
+  "remove 4 spaces from beginning of of line"
+  (interactive)
+  (save-excursion
+    (save-match-data
+      (beginning-of-line)
+      ;; get rid of tabs at beginning of line
+      (when (looking-at "^\\s-+")
+        (untabify (match-beginning 0) (match-end 0)))
+      (when (looking-at "^    ")
+        (replace-match "")))))
+
+ (require 'mmm-mode)
+ (setq mmm-global-mode 'maybe)
+     (mmm-add-mode-ext-class 'html-mode "\\.php\\'" 'html-php)
